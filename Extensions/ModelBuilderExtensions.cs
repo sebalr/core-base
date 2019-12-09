c5df2370dfda2e98320787993d26538408c0af28
sebalr <larrieu.sebastian@gmail.com>
2019-12-09 20:59:34 -0300

    Shorter version of static

--- Extensions/ModelBuilderExtensions.cs
+++ Extensions/ModelBuilderExtensions.cs
@@ -8,27 +8,16 @@ namespace CoreBase.Extensions
     {
         public static void ConfigureFromAllAssembly(this ModelBuilder modelBuilder)
         {
-            var applyGenericMethod = typeof(ModelBuilder).GetMethods().Where(m => m.Name == "ApplyConfiguration" && m.GetParameters().First().ParameterType.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>).GetGenericTypeDefinition()).FirstOrDefault();
-            // replace GetExecutingAssembly with assembly where your configurations are if necessary
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
             foreach (var assembly in assemblies)
             {
-                foreach (var type in assembly.GetTypes()
-                        .Where(c => c.IsClass && !c.IsAbstract && !c.ContainsGenericParameters))
+                var typesToRegister = assembly.GetTypes().Where(t => t.GetInterfaces()
+                    .Any(gi => gi.IsGenericType && gi.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>))).ToList();
+
+                foreach (var type in typesToRegister)
                 {
-                    // use type.Namespace to filter by namespace if necessary
-                    foreach (var iface in type.GetInterfaces())
-                    {
-                        // if type implements interface IEntityTypeConfiguration<SomeEntity>
-                        if (iface.IsConstructedGenericType && iface.GetGenericTypeDefinition() == typeof(IEntityTypeConfiguration<>))
-                        {
-                            // make concrete ApplyConfiguration<SomeEntity> method
-                            var applyConcreteMethod = applyGenericMethod.MakeGenericMethod(iface.GenericTypeArguments[0]);
-                            // and invoke that with fresh instance of your configuration type
-                            applyConcreteMethod.Invoke(modelBuilder, new object[] { Activator.CreateInstance(type) });
-                            break;
-                        }
-                    }
+                    dynamic configurationInstance = Activator.CreateInstance(type);
+                    modelBuilder.ApplyConfiguration(configurationInstance);
                 }
             }
         }
